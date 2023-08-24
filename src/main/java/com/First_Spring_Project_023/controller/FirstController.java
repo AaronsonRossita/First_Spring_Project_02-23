50bcb3f2e474bc49f54da82e43bf34fed036f1d6
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-08-24 21:12:23 +0300

    CRUD

--- src/main/java/com/First_Spring_Project_023/controller/FirstController.java
+++ src/main/java/com/First_Spring_Project_023/controller/FirstController.java
@@ -1,24 +1,45 @@
 package com.First_Spring_Project_023.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.First_Spring_Project_023.model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/first")
 public class FirstController {
 
+    // Create Read Update Delete
+    //  POST  GET   PUT   DELETE
 
-    @GetMapping(value = "/hello")
-    public static String getFunction(){
-        return "Hi";
+    @GetMapping(value = "/first_get")
+    public static String firstGet(){
+        return "This is the first get";
     }
 
-    @GetMapping(value = "/return/{s}")
-    public static String returnFunction(@PathVariable String s){
-        System.out.println("STRING");
-        return s;
+    @GetMapping(value = "/second_get/{name}")
+    public static String secondGet(@PathVariable String name){
+        return "You wrote " + name;
+    }
+
+    @GetMapping(value = "/third_get")
+    public static String thirdGet(@RequestParam String name){
+        return "You sent me this name " + name;
+    }
+
+    @GetMapping(value = "/fourth_get")
+    public static String fourthGet(@RequestParam int id){
+        return "You sent me this id " + id;
+    }
+
+    @PostMapping(value = "first_post")
+    public static String firstPost(@RequestBody String name){
+        return "You posted this name : " + name;
+    }
+
+    @PostMapping(value = "second_post")
+    public static String secondPost(@RequestBody Customer customer){
+        return customer.toString();
     }
 
 
---
+++ src/main/java/com/First_Spring_Project_023/model/Customer.java
@@ -0,0 +1,48 @@
+package com.First_Spring_Project_023.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Customer {
+
+    @JsonProperty("id")
+    private int customerId;
+    @JsonProperty("name")
+    private String customerName;
+    @JsonProperty("phone")
+    private String customerPhone;
+
+    public Customer(int id, String name, String phone) {
+        this.customerId = id;
+        this.customerName = name;
+        this.customerPhone = phone;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is a customer with id %s and phone number %s",this.customerName,this.customerId,this.customerPhone);
+    }
+}
